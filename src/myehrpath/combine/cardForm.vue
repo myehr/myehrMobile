ed67dc10a9a0d83e4e4a247d57c417033fa9576c
yinheng <yinheng@you-fuli.com>
2018-11-05 21:56:28 +0800

    'dd'

---
+++ src/myehrpath/combine/cardForm.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <test-card-form :compParams="params"></test-card-form>
+    </div>
+</template>
+
+<script>
+
+    import  testCardForm from '@/myehrpath/testCardForm.vue'
+
+    export default {
+        name: "cardForm",
+        components: {
+          testCardForm
+        },data(){
+          return {
+            params:{"EMPEMPLOYEE_EMPID":"258"}
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
